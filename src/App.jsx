56553f7a0ef577d2965c7df8901a34174d2e6de1
eric arieta <earrieta21@gmail.com>
2023-07-04 18:54:39 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+     
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+     
